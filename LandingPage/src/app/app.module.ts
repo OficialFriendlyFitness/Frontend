a392c0798c2ff7e2e3f579c82130c8f60e71c9ed
paoloVD21 <pvizconded@hotmail.com>
2023-11-11 02:19:08 -0500

    Landing Page

---
+++ LandingPage/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import {NgOptimizedImage} from "@angular/common";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
